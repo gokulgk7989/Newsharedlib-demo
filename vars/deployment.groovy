939f252029f6a2f1222514251f3beabe69837c92
gokulgk7989 <112245233+gokulgk7989@users.noreply.github.com>
2023-01-13 13:20:33 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,5 +1,5 @@
-def call() {
-      sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@172.31.44.98:/opt/tomcat9/webapps"
-      sh "ssh ec2-user@172.31.44.98 sudo /opt/tomcat9/bin/shutdown.sh"
-      sh "ssh ec2-user@172.31.44.98 sudo /opt/tomcat9/bin/startup.sh"
+def call(user,ip) {
+      sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps"
+      sh "ssh ${user}@${ip} sudo /opt/tomcat9/bin/shutdown.sh"
+      sh "ssh ${user}@${ip} sudo /opt/tomcat9/bin/startup.sh"
 }
